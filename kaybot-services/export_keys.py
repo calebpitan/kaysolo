6813b6602d68764ca97f35133eab9bbc23cc6a7b
Caleb Adepitan <calebpitan@gmail.com>
2023-08-12 19:14:45 +0100

    chore: run through formatter

--- kaybot-services/export_keys.py
+++ kaybot-services/export_keys.py
@@ -3,34 +3,40 @@ from subprocess import run
 
 from dotenv import dotenv_values
 
-JWT_RS256_PUB_KEY = 'JWT_RS256_PUB_KEY'
-JWT_RS256_KEY = 'JWT_RS256_KEY'
+JWT_RS256_PUB_KEY = "JWT_RS256_PUB_KEY"
+JWT_RS256_KEY = "JWT_RS256_KEY"
+
 
 def format(data: str):
-    return '"{transformed_data}"\n'.format(transformed_data = data.strip().replace('\n', '\\n'))
+    return '"{transformed_data}"\n'.format(
+        transformed_data=data.strip().replace("\n", "\\n")
+    )
+
 
 def save_env_file(data_dict: dict[str, str | None], file_path: str):
-    with open(file_path, 'w') as env_file:
+    with open(file_path, "w") as env_file:
         for key, value in data_dict.items():
-            env_file.write(f'{key}={value}\n')
+            env_file.write(f"{key}={value}\n")
+
 
 def main():
-    key_generation_script_path = abspath('../scripts/jwtrs256.sh')
-    pubkfile_path = abspath('.keychain/jwtRS256.key.pub')
-    pkfile_path = abspath('.keychain/jwtRS256.key')
-    envfile_path = abspath('.env')
+    key_generation_script_path = abspath("../scripts/jwtrs256.sh")
+    pubkfile_path = abspath(".keychain/jwtRS256.key.pub")
+    pkfile_path = abspath(".keychain/jwtRS256.key")
+    envfile_path = abspath(".env")
 
     env_dict = dotenv_values(envfile_path)
 
-    run(['bash', key_generation_script_path], check=True)
+    run(["bash", key_generation_script_path], check=True)
 
-    with open(pubkfile_path, 'r') as pubkfile:
-        env_dict.update({ JWT_RS256_PUB_KEY: format(pubkfile.read()) })
+    with open(pubkfile_path, "r") as pubkfile:
+        env_dict.update({JWT_RS256_PUB_KEY: format(pubkfile.read())})
 
-    with open(pkfile_path, 'r') as pkfile:
-        env_dict.update({ JWT_RS256_KEY: format(pkfile.read()) })
+    with open(pkfile_path, "r") as pkfile:
+        env_dict.update({JWT_RS256_KEY: format(pkfile.read())})
 
     save_env_file(env_dict, envfile_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
