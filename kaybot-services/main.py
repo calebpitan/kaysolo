1d4fa8b8a4526a474fd800ae141e12f43acfdbdf
Caleb Adepitan <calebpitan@gmail.com>
2023-08-12 19:48:23 +0100

    chore: write initial application startup script

---
+++ kaybot-services/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from core.settings import kaybot_settings
+
+
+app = FastAPI(
+    title=kaybot_settings.APP_NAME,
+    description=kaybot_settings.DESCRIPTION,
+    version=kaybot_settings.VERSION,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_credentials=True,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=kaybot_settings.PORT, reload=True)
