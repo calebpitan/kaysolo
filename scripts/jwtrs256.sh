8a2fba5e7ee46f78e5a1f8a0b57bced09981e308
Caleb Adepitan <calebpitan@gmail.com>
2023-08-12 18:27:43 +0100

    chore: add key-pair generation script

---
+++ scripts/jwtrs256.sh
@@ -0,0 +1,4 @@
+mkdir .keychain
+ssh-keygen -t rsa -b 2048 -m PEM -f .keychain/jwtRS256.key
+# Don't add passphrase
+openssl rsa -in .keychain/jwtRS256.key -pubout -outform PEM -out .keychain/jwtRS256.key.pub
