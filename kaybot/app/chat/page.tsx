061d8b72e273f51aef373ed889db0cc82dc36a8d
Caleb Adepitan <calebpitan@gmail.com>
2023-08-14 13:40:40 +0100

    update: implement chat message send

--- kaybot/app/chat/page.tsx
+++ kaybot/app/chat/page.tsx
@@ -1,15 +1,63 @@
-import { Flex } from '@/chakra-ui/react';
+'use client';
+
+import { Box, Flex } from '@/chakra-ui/react';
 
 import { ChatBox } from '@/core/components/ChatBox';
 import { ChatTextBox } from '@/core/components/ChatTextBox';
 import { ChatLayout } from '@/core/composition/ChatLayout';
+import { useSendMessage } from '@/core/services';
+import { useState } from 'react';
+
+export interface ChatDialogue {
+  role: 'assistant' | 'user';
+  message: string;
+  responsed?: boolean;
+}
 
 const Chat = () => {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState<ChatDialogue[]>([]);
+  const handlePostChatMessage = useSendMessage();
+
+  const handleSend = async () => {
+    const outbound_message = message;
+
+    setChat([...chat, { role: 'user', message: outbound_message, responsed: false }]);
+    setMessage('');
+
+    const response = await handlePostChatMessage.mutateAsync({
+      message_body: outbound_message,
+    });
+
+    const choices = response.data.response.choices;
+
+    const newChat = chat.slice(0, chat.length - 1);
+
+    newChat.push({ ...chat.at(-1)!, responsed: true });
+    newChat.push({ role: 'assistant', message: choices.at(0)!.message.content });
+
+    setChat(newChat);
+  };
+
+  const handleChange = (message: string) => {
+    setMessage(message);
+  };
+
   return (
     <ChatLayout>
       <Flex justifyContent="center" height="full">
-        <ChatBox px={3} justifyContent="center" alignItems="flex-end">
-          <ChatTextBox mb={8} />
+        <ChatBox px={3} justifyContent="center" alignItems="flex-end" flexDirection="column">
+          <Flex flexDirection="column" width="full" height="full" pt={4} px={3} fontSize="sm">
+            {chat.map((c) => {
+              return (
+                <Box key={c.message} bgColor="whiteAlpha.600">
+                  {c.message}
+                </Box>
+              );
+            })}
+          </Flex>
+
+          <ChatTextBox mb={8} onChange={handleChange} onSend={handleSend} />
         </ChatBox>
       </Flex>
     </ChatLayout>
--- kaybot/core/components/ChatTextBox/ChatTextBox.tsx
+++ kaybot/core/components/ChatTextBox/ChatTextBox.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { Box, FlexProps, Flex, SystemStyleObject, Textarea, IconButton, Icon } from '@/chakra-ui/react';
-import { ChangeEvent, FocusEvent, useState } from 'react';
+import { ChangeEvent, MouseEvent, useState } from 'react';
 import { IoSend } from 'react-icons/io5';
 
 import styles from './index.module.css';
 
-export interface ChatTextBoxProps extends FlexProps {}
+export interface ChatTextBoxProps extends Omit<FlexProps, 'onChange'> {
+  onChange?: (message: string) => void;
+  onSend?: () => void;
+}
 
-export const ChatTextBox = (props: ChatTextBoxProps) => {
+export const ChatTextBox = ({ onChange, onSend, ...props }: ChatTextBoxProps) => {
   const [value, setValue] = useState('');
   const sty: SystemStyleObject = {
     py: 0,
@@ -23,8 +26,13 @@ export const ChatTextBox = (props: ChatTextBoxProps) => {
     if (!box) return;
 
     setValue(evt.target.value);
+    onChange?.(evt.target.value);
 
     box.dataset.value = evt.target.value;
+  };
+
+  const handleSend = (_evt: MouseEvent<HTMLButtonElement>) => {
+    onSend?.();
   };
 
   return (
@@ -75,7 +83,7 @@ export const ChatTextBox = (props: ChatTextBoxProps) => {
       </Box>
 
       <Box alignSelf="flex-end" ms={3} position="sticky" top={'calc(100% - 2rem)'} right={0}>
-        <IconButton aria-label="Send" borderRadius="xl" size="sm">
+        <IconButton aria-label="Send" borderRadius="xl" size="sm" onClick={handleSend}>
           <Icon as={IoSend} />
         </IconButton>
       </Box>
