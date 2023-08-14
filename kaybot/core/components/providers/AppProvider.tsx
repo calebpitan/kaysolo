d7233e5a5503de1dc2e6970e9e73d7ebc8831538
Caleb Adepitan <calebpitan@gmail.com>
2023-08-14 13:41:00 +0100

    chore: add query client and query client provider

--- kaybot/core/components/providers/AppProvider.tsx
+++ kaybot/core/components/providers/AppProvider.tsx
@@ -4,20 +4,38 @@ import { ChakraProvider, ColorModeScript } from '@/chakra-ui/react';
 import { theme } from '@/core/theme/theme';
 
 import { CacheProvider } from '@chakra-ui/next-js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Fragment, ReactNode } from 'react';
 
 export interface AppProviderProps {
   children?: ReactNode;
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryKeyHashFn: (queryKey) => {
+        return JSON.stringify(queryKey, (_, value) => {
+          if (value instanceof Set) {
+            return Array.from(value);
+          }
+          return value;
+        });
+      },
+    },
+  },
+});
+
 export function AppProvider({ children }: AppProviderProps) {
   return (
     <Fragment>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
 
-      <CacheProvider>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-      </CacheProvider>
+      <QueryClientProvider client={queryClient}>
+        <CacheProvider>
+          <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </CacheProvider>
+      </QueryClientProvider>
     </Fragment>
   );
 }
