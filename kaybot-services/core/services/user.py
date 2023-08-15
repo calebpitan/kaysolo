af54c7fb19a30bff7a0747f48478e3b1d745c3d3
Caleb Adepitan <calebpitan@gmail.com>
2023-08-15 12:59:26 +0100

    chore: refactor wrong reference

--- kaybot-services/core/services/user.py
+++ kaybot-services/core/services/user.py
@@ -9,4 +9,4 @@ async def get_user_by_id(db: Session, id: UUID4):
 
 
 async def get_user_by_username(db: Session, username: str):
-    return db.query(model.Account).filter(model.User.username == username)
+    return db.query(model.User).filter(model.User.username == username)
