baa877941e0ae259ce039d7d3131d2171506f22f
Caleb Adepitan <calebpitan@gmail.com>
2023-08-15 22:44:32 +0100

    chore: remove delcarative base from here

--- kaybot-services/core/database/engine.py
+++ kaybot-services/core/database/engine.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 from core.settings import kaybot_settings as s
 
 DATABASE_URL = f"postgresql://{s.POSTGRES_USER}:{s.POSTGRES_PASSWORD}@{s.POSTGRES_HOST}/{s.APP_CODE_NAME}"
 
 engine = create_engine(DATABASE_URL, echo=True)
-
-Base = declarative_base()
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
