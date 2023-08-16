1ee7ebd7d97f19443f773658e9bcf5212f347901
Caleb Adepitan <calebpitan@gmail.com>
2023-08-16 21:38:43 +0100

    chore: update
    - improve implementation
    - raise HTTPException when username or email is already in use
    - drop async usage

--- kaybot-services/core/services/account.py
+++ kaybot-services/core/services/account.py
@@ -1,25 +1,57 @@
+from fastapi import HTTPException
 from pydantic import UUID4, EmailStr
 from sqlalchemy.orm import Session
 
 from core.models import account as model, user as user_model
 from core.schemas import account as schema
+from core.utils import create_error
+
+from .user import get_user_by_username
 
 
-async def create_account(db: Session, account: schema.AccountCreate):
-    password = account.password
-    user = user_model.User(**account.user.model_dump())
-    account = model.Account(email=account.email, password=password, user=user)
+def create_account_service(session: Session, credentials: schema.AccountCreate):
+    password = credentials.password
+    user = user_model.User(**credentials.user.model_dump())
+    account = model.Account(email=credentials.email, password=password, user=user)
 
-    db.add(account)
-    db.commit()
-    db.refresh(account)
+    existing_account = get_account_by_email(
+        session,
+        email=credentials.email,
+    ).one_or_none()
+
+    existing_user = get_user_by_username(
+        session,
+        username=credentials.user.username,
+    ).one_or_none()
+
+    if existing_account:
+        raise HTTPException(
+            400,
+            create_error(
+                message=f"email address [{credentials.email}] is already in use"
+            ),
+        )
+
+    if existing_user:
+        raise HTTPException(
+            400,
+            create_error(
+                message=f"username [{credentials.user.username}] is already in use"
+            ),
+        )
+
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+
+    print(account.user)
 
     return account
 
 
-async def get_account_by_id(db: Session, id: UUID4):
-    return db.query(model.Account).filter(model.Account.id == id)
+def get_account_by_id(session: Session, id: UUID4):
+    return session.query(model.Account).filter(model.Account.id == id)
 
 
-async def get_account_by_email(db: Session, email: EmailStr):
-    return db.query(model.Account).filter(model.Account.email == email)
+def get_account_by_email(session: Session, email: EmailStr):
+    return session.query(model.Account).filter(model.Account.email == email)
--- kaybot-services/core/services/user.py
+++ kaybot-services/core/services/user.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import Session
 from core.models import user as model
 
 
-async def get_user_by_id(db: Session, id: UUID4):
-    return db.query(model.User).filter(model.User.id == id)
+def get_user_by_id(session: Session, id: UUID4):
+    return session.query(model.User).filter(model.User.id == id)
 
 
-async def get_user_by_username(db: Session, username: str):
-    return db.query(model.User).filter(model.User.username == username)
+def get_user_by_username(session: Session, username: str):
+    return session.query(model.User).filter(model.User.username == username)
