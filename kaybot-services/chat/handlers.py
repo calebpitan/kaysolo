003de4dea63952fa229d242958062326220d7814
Caleb Adepitan <calebpitan@gmail.com>
2023-08-16 21:31:34 +0100

    chore: update chat handler
    - rename operation
    - drop async usage
    - update imports

--- kaybot-services/chat/handlers.py
+++ kaybot-services/chat/handlers.py
@@ -3,14 +3,14 @@ import openai
 from fastapi import HTTPException
 
 from chat.router import router
-from intelligence.basic import generate_prompt, generate_response, PersonalityBackground
+from core.schemas.chat import MessageCreate
 from core.schemas.openai import ChatCompletionResponse
-from core.schemas.chat import MessageSend
 from core.utils import create_error
+from intelligence.basic import generate_prompt, generate_response, PersonalityBackground
 
 
 @router.post("/message", response_model=ChatCompletionResponse)
-async def chat(message: MessageSend) -> dict:
+def create_message(message: MessageCreate) -> dict:
     """
     An endpoint to post messages and expect a response. The messages are reconstructed into a
     valuable prompt and sent over to OpenAI's API which most likely responds with a message.
