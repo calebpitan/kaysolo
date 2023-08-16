ebb7d489551ea660a634bd7a80544b27d57d751a
Caleb Adepitan <calebpitan@gmail.com>
2023-08-16 22:31:37 +0100

    chore: add client addresses for allowed origins

--- kaybot-services/core/settings.py
+++ kaybot-services/core/settings.py
@@ -11,6 +11,8 @@ class Settings(BaseSettings):
 
     DESCRIPTION: str = "KayBot is a chatbot that can be utilized for lead generation in the field of digital marketing."
 
+    CLIENT_ADDRESSES: list[str] = config("CLIENT_ADDRESSES", cast=str).split(",")
+
     JWT_RS256_PUB_KEY: str = config("JWT_RS256_PUB_KEY", cast=str)
     JWT_RS256_KEY: str = config("JWT_RS256_KEY", cast=str)
     TOKEN_EXPIRY: str = config("TOKEN_EXPIRY", cast=str)
