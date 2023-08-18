cd50332fb0500f4c7fd9e6e52b659375b98d6b3d
Caleb Adepitan <calebpitan@gmail.com>
2023-08-18 15:11:50 +0100

    chore: fix issues in script
    - unnecessary `shift`-ing
    - wrap `$@` in quotes to prevent misinterpretation of arguments

--- kaybot-services/run_migrations.sh
+++ kaybot-services/run_migrations.sh
@@ -2,12 +2,9 @@
 
 echo "running migrations commands..."
 
-# shift the $@ args by one to skip $0 which is the name of the running executable
-shift
-
 # whatever args are passed to this script when run are redirected to the
 # `alembic revision --autogenerate` command
-python -m alembic revision --autogenerate $@ # -m "create inital tables"
+python -m alembic revision --autogenerate "$@" # -m "create inital tables"
 python -m alembic upgrade head
 
 echo "applied migrations!"
