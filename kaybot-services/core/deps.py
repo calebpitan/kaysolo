18b91bcffb8ffe4128bf2c242de602c6cc0403f5
Caleb Adepitan <calebpitan@gmail.com>
2023-08-19 19:18:15 +0100

    update: add implementation for loading the session user

--- kaybot-services/core/deps.py
+++ kaybot-services/core/deps.py
@@ -1,3 +1,15 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from jose import JWTError
+from sqlalchemy.orm import Session
+
+from core.authentication.oauth2 import oauth2_scheme
+from core.services.account import get_account_by_id
+from core.schemas.account import TokenData
+from core.utils import create_error
+
+from .authentication.token import JWTRS256Token, get_account_id_from_sub
 from .database.engine import SessionLocal
 
 
@@ -9,3 +21,41 @@ def get_db():
         db.rollback()
     finally:
         db.close()
+
+
+def get_current_account(
+    token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=create_error(message="Could not validate credentials"),
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    encoded_token = JWTRS256Token[dict[str, str]](token=token)
+
+    try:
+        payload = encoded_token.decode()
+
+        account_id = payload.get("sub")
+
+        if account_id is None:
+            raise credentials_exception
+
+        # reassign: parse the sub header containing prefix "account_id:"
+        account_id = get_account_id_from_sub(account_id)
+
+        token_data = TokenData(account_id=account_id)
+
+    except JWTError:
+        raise credentials_exception
+
+    try:
+        account = get_account_by_id(db, id=token_data.account_id)
+    except HTTPException as exc:
+        if exc.status_code == status.HTTP_404_NOT_FOUND:
+            raise credentials_exception
+        else:
+            raise exc
+
+    return account
