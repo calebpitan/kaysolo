13e7918f42f776077050539b6618069a7c532f67
Caleb Adepitan <calebpitan@gmail.com>
2023-08-19 18:45:37 +0100

    chore: update implementations and add more route handlers
    - add route and handler for OAuth2 authentication
    - add route and handler for identifying the user with the current session

--- kaybot-services/account/handlers.py
+++ kaybot-services/account/handlers.py
@@ -1,19 +1,49 @@
+from datetime import timedelta
+from typing import Annotated
+
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from core.deps import get_db
-from core.schemas.account import Account, AccountCreate
-from core.services.account import create_account_service
+from core.authentication.token import JWTRS256Token, account_id_sub
+from core.deps import get_db, get_current_account
+from core.settings import settings
+from core.schemas.account import Account, AccountCreate, Token, TokenTypeEnum
+from core.services.account import (
+    authenticate_user_account_service,
+    create_user_account_service,
+)
 
 from .router import router
 
 
 @router.post("/create", response_model=Account)
-def create_account(credentials: AccountCreate, db: Session = Depends(get_db)):
-    """Create a user account that will be used for authentication and will serve
-    as the user's identification through the entire system
-    """
-
-    account = create_account_service(session=db, credentials=credentials)
+def create_user_account(credentials: AccountCreate, db: Session = Depends(get_db)):
+    account = create_user_account_service(session=db, credentials=credentials)
 
     return account
+
+
+@router.post("/authenticate", response_model=Token)
+def authenticate_user_account(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db),
+):
+    account = authenticate_user_account_service(
+        db,
+        identifier=form_data.username,
+        password=form_data.password,
+    )
+
+    access_token = JWTRS256Token.from_data(data={"sub": account_id_sub(account.id.hex)})
+
+    return Token(access_token=access_token, token_type=TokenTypeEnum.BEARER)
+
+
+@router.get("/me", response_model=Account)
+def identify_user_account(
+    current_account: Annotated[Account, Depends(get_current_account)]
+):
+    """Identify the current user and return the user account"""
+
+    return current_account
