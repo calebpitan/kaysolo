1b78f05fa728223769520366c076af15aa5f6c70
Caleb Adepitan <calebpitan@gmail.com>
2023-08-20 14:58:54 +0100

    update: add react-toastify toast container

--- kaybot/core/components/providers/AppProvider.tsx
+++ kaybot/core/components/providers/AppProvider.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { CacheProvider } from '@/chakra-ui/next-js';
 import { ChakraProvider, ColorModeScript } from '@/chakra-ui/react';
+
 import { theme } from '@/core/theme/theme';
 
-import { CacheProvider } from '@chakra-ui/next-js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Fragment, ReactNode } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 export interface AppProviderProps {
   children?: ReactNode;
@@ -33,7 +35,16 @@ export function AppProvider({ children }: AppProviderProps) {
 
       <QueryClientProvider client={queryClient}>
         <CacheProvider>
-          <ChakraProvider theme={theme}>{children}</ChakraProvider>
+          <ChakraProvider theme={theme}>
+            {children}
+
+            <ToastContainer
+              className="Toastify-container--customized"
+              hideProgressBar={true}
+              closeButton={false}
+              icon={false}
+            />
+          </ChakraProvider>
         </CacheProvider>
       </QueryClientProvider>
     </Fragment>
