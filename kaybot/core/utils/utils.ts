2e59e56d8c3acbec9bc4806434dc07e3aff12880
Caleb Adepitan <calebpitan@gmail.com>
2023-08-21 01:44:55 +0100

    chore: add utility functions as needed

--- kaybot/core/utils/utils.ts
+++ kaybot/core/utils/utils.ts
@@ -8,8 +8,25 @@ import { theme } from '../theme';
 
 export const APP_BAR_HEIGHT = 50;
 export const USERNAME_REGEX = /^[A-Za-z]+(?:[_.]?[A-Za-z0-9]+)*$/;
+export const ACCESS_TOKEN_KEY = 'access_token';
+export const REFRESH_TOKEN_KEY = 'refresh_token';
 
 export const getThemeColor = <T = unknown, L extends Choose<ThemeTypings, 'colors'> = string>(label: L) => {
   const field = get(theme.colors, label);
   return field ? (field as T) : label;
+};
+
+export const storeToken = (token: string, key = ACCESS_TOKEN_KEY) => {
+  if (typeof window === 'undefined') return;
+  localStorage.setItem(key, token);
+};
+
+export const getToken = (key = ACCESS_TOKEN_KEY) => {
+  if (typeof window === 'undefined') return null;
+  return localStorage.getItem(key);
+};
+
+export const removeToken = (key = ACCESS_TOKEN_KEY) => {
+  if (typeof window === 'undefined') return;
+  return localStorage.removeItem(key);
 };
