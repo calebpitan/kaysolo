e0598e89eca360d47b2aa9dc5b3cb71c9866c89c
Caleb Adepitan <calebpitan@gmail.com>
2023-08-23 11:55:46 +0100

    update: define how server base url is resolved
    - when connected to a wifi, the wiif address could be used to remotely
    access the app on the same network, in that case localhost won't work, as
    it refers to the host machine itself trying to access the remote server,
    rather than the remote server

--- kaybot/core/services/config.ts
+++ kaybot/core/services/config.ts
@@ -2,10 +2,21 @@ import { Configuration } from '@/client';
 
 import { getToken } from '../utils';
 
-export const SERVER_BASE_URL = process.env.SERVER_BASE_URL;
+export const SERVER_BASE_URL = process.env.SERVER_BASE_URL!;
 
 export class MissingAccessTokenException extends Error {
   name = MissingAccessTokenException.name;
+}
+
+function getServerBaseURL() {
+  if (process.env.NODE_ENV !== 'development' || typeof window === 'undefined') return SERVER_BASE_URL;
+
+  const clientAddress = new URL(window.location.href);
+  const serverAddress = new URL(SERVER_BASE_URL);
+
+  const serverBaseUrl = `${clientAddress.protocol}//${clientAddress.hostname}:${serverAddress.port}`;
+
+  return serverBaseUrl;
 }
 
 function getAccessToken() {
@@ -16,4 +27,4 @@ function getAccessToken() {
   return Promise.resolve(token);
 }
 
-export const configuration = new Configuration({ basePath: SERVER_BASE_URL, accessToken: getAccessToken });
+export const configuration = new Configuration({ basePath: getServerBaseURL(), accessToken: getAccessToken });
