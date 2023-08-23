6e8da2245cf57b519988efe54b066fd2060b345a
Caleb Adepitan <calebpitan@gmail.com>
2023-08-23 02:19:29 +0100

    update
    - improve logic
    - gated route using `PrivateRoute`

--- kaybot/app/chat/page.tsx
+++ kaybot/app/chat/page.tsx
@@ -3,12 +3,15 @@
 import { Box, Flex, VStack } from '@/chakra-ui/react';
 
 import { ChatBox } from '@/core/components/ChatBox';
+import { ChatMesssage } from '@/core/components/ChatMessage';
 import { ChatTextBox } from '@/core/components/ChatTextBox';
+import { ConfigContext, PrivateRoute } from '@/core/components/Providers';
 import { ChatLayout } from '@/core/composition/ChatLayout';
 import { useSendMessage } from '@/core/services';
-import { useState } from 'react';
+import { fullname } from '@/core/utils';
+import { useContext, useState } from 'react';
 
-export interface ChatDialogue {
+export interface Conversation {
   role: 'assistant' | 'user';
   message: string;
   responsed?: boolean;
@@ -16,13 +19,25 @@ export interface ChatDialogue {
 
 const Chat = () => {
   const [message, setMessage] = useState('');
-  const [chat, setChat] = useState<ChatDialogue[]>([]);
+  const [conversations, setConversations] = useState<Conversation[]>([]);
+
+  const { config } = useContext(ConfigContext);
+  const user = config.session.user_account?.user;
+
   const handlePostChatMessage = useSendMessage();
+
+  const handleChange = (message: string) => {
+    setMessage(message);
+  };
 
   const handleSend = async () => {
     const outbound_message = message;
 
-    setChat([...chat, { role: 'user', message: outbound_message, responsed: false }]);
+    const newConversations = conversations.slice();
+
+    newConversations.push({ role: 'user', message: outbound_message, responsed: false });
+
+    setConversations(newConversations);
     setMessage('');
 
     const response = await handlePostChatMessage.mutateAsync({
@@ -31,36 +46,32 @@ const Chat = () => {
 
     const choices = response.data.response.choices;
 
-    const newChat = chat.slice(0, chat.length - 1);
+    const newConversationsWithResponse = newConversations.slice(0, newConversations.length - 1);
 
-    newChat.push({ ...chat.at(-1)!, responsed: true });
-    newChat.push({ role: 'assistant', message: choices.at(0)!.message.content });
+    newConversationsWithResponse.push({ ...newConversations.at(-1)!, responsed: true });
+    newConversationsWithResponse.push({ role: 'assistant', message: choices.at(0)!.message.content });
 
-    setChat(newChat);
-  };
-
-  const handleChange = (message: string) => {
-    setMessage(message);
+    setConversations(newConversationsWithResponse);
   };
 
   return (
-    <ChatLayout>
-      <Flex justifyContent="center" height="full">
-        <ChatBox px={3} justifyContent="center" alignItems="flex-end" flexDirection="column">
-          <VStack width="full" height="full" alignItems="flex-start" py={4} px={1} fontSize="sm" overflow="auto">
-            {chat.map((c) => {
-              return (
-                <Box key={c.message} p={3} bgColor="whiteAlpha.600" borderRadius="2xl" whiteSpace="pre-wrap">
-                  {c.message}
-                </Box>
-              );
-            })}
-          </VStack>
+    <PrivateRoute>
+      <ChatLayout name={user ? fullname(user) : ''}>
+        <Flex justifyContent="center" height="full">
+          <ChatBox justifyContent="center" alignItems="flex-end" flexDirection="column">
+            <VStack width="full" height="full" alignItems="flex-start" py={4} fontSize="md" overflow="auto">
+              {conversations.map((c, i) => {
+                return <ChatMesssage key={i} message={c.message} role={c.role} color="white" />;
+              })}
+            </VStack>
 
-          <ChatTextBox my={8} onChange={handleChange} onSend={handleSend} />
-        </ChatBox>
-      </Flex>
-    </ChatLayout>
+            <Box px={3} width="full">
+              <ChatTextBox my={8} value={message} onChange={handleChange} onSend={handleSend} />
+            </Box>
+          </ChatBox>
+        </Flex>
+      </ChatLayout>
+    </PrivateRoute>
   );
 };
 
