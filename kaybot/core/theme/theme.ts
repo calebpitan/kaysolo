4ce9413c60345c510e430c7f1131da9397a4504b
Caleb Adepitan <calebpitan@gmail.com>
2023-08-26 14:23:19 +0100

    update app settungs

--- kaybot/core/theme/theme.ts
+++ kaybot/core/theme/theme.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   ComponentStyleConfig,
   extendTheme,
---
+++ kaybot/core/utils/settings.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.APP_NAME!;
+
+export const SERVER_BASE_URL = process.env.SERVER_BASE_URL!;
+export const SERVER_INTERNAL_ADDRESS = process.env.SERVER_INTERNAL_ADDRESS!;
+
+export const CONFIG_SCRIPT_NAME = 'config';
