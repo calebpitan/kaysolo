219da2d93c5d8e56614713cd0e2e3eedebd4dfdc
Caleb Adepitan <calebpitan@gmail.com>
2023-08-26 14:24:31 +0100

    update chat layout

--- kaybot/core/components/ProfileMenu/ProfileMenu.tsx
+++ kaybot/core/components/ProfileMenu/ProfileMenu.tsx
@@ -1,12 +1,23 @@
+'use client';
+
 import { Avatar, HStack, StackProps } from '@/chakra-ui/react';
 
+import { fullname } from '@/core/utils';
+
+import { useContext } from 'react';
+
+import { ConfigContext } from '../Providers';
 import { Typography } from '../Typography';
 
 export interface ProfileMenuProps extends StackProps {
-  name: string;
+  name?: string;
 }
 
-export const ProfileMenu = ({ name = 'Caleb Adepitan', ...rest }: ProfileMenuProps) => {
+export const ProfileMenu = ({ name, ...rest }: ProfileMenuProps) => {
+  const { config } = useContext(ConfigContext);
+  const user = config.session.user_account?.user;
+  const fname = name ? name : user ? fullname(user) : '';
+
   return (
     <HStack
       ps={2}
@@ -15,15 +26,17 @@ export const ProfileMenu = ({ name = 'Caleb Adepitan', ...rest }: ProfileMenuPro
       backdropFilter="auto"
       backdropBlur="xl"
       backdropSaturate="180%"
-      bgColor="whiteAlpha.400"
+      bgColor="whiteAlpha.300"
       borderRadius="full"
       width={{ base: 'auto', lg: 250 }}
-      _hover={{ bgColor: 'whiteAlpha.300' }}
+      _hover={{ bgColor: 'whiteAlpha.400' }}
       {...rest}
     >
-      <Avatar name={name} size={{ base: 'sm', lg: 'md' }} />
+      <Avatar name={fname} size={{ base: 'sm', lg: 'md' }} />
 
-      <Typography fontWeight={600}>{name}</Typography>
+      <Typography fontWeight={600} color="gray.100">
+        {fname}
+      </Typography>
     </HStack>
   );
 };
--- kaybot/core/composition/ChatLayout/ChatLayout.tsx
+++ kaybot/core/composition/ChatLayout/ChatLayout.tsx
@@ -2,8 +2,10 @@ import { Box, Flex } from '@/chakra-ui/react';
 
 import { AppBar } from '@/core/components/AppBar';
 import { ProfileMenu } from '@/core/components/ProfileMenu';
+import { ConfigContext } from '@/core/components/Providers';
 import { APP_BAR_HEIGHT } from '@/core/utils';
-import { ReactNode } from 'react';
+
+import { ReactNode, useContext } from 'react';
 
 export interface ChatLayoutProps {
   name: string;
@@ -11,9 +13,11 @@ export interface ChatLayoutProps {
 }
 
 export const ChatLayout = ({ children, name }: ChatLayoutProps) => {
+  const { config } = useContext(ConfigContext);
+
   return (
     <Box height="full" bgImage="url(/images/chat-background.png)" bgRepeat="no-repeat" bgSize="cover">
-      <AppBar color="whiteAlpha.600" />
+      <AppBar color="whiteAlpha.600" title={config.application_config.name} />
 
       <Flex height={`calc(100% - ${APP_BAR_HEIGHT}px)`} width="full">
         <Box px={6} py={8} flexShrink={0} alignSelf="flex-end" display={{ base: 'none', lg: 'block' }}>
